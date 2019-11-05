f4a360ff417d4be9072c75928742a4013813bfb2
ecustovic <emina.custovic@edu.fit.ba>
2019-11-05 16:04:36 +0100

    tetsing movie model

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class MovieTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @movie = movies(:batman)
+  end
+  
+  test "batman fixture is valid" do
+    assert @movie.valid?
+  end
+
+  test "#released, returns latest realeased movies" do
+    result = Movie.released 
+    expected_results = Movie.where("released_on < ?", Time.now).order("released_on desc")
+    
+    assert_equal result.map(&:id), expected_results.map(&:id)
+  end
+
+  test "#upcoming, returns movies realeased in future" do
+    result = Movie.upcoming 
+    expected_results = Movie.where("released_on > ?", Time.now).order("released_on asc")
+    
+    assert_equal result.map(&:id), expected_results.map(&:id)
+
+  end
+  
 end
