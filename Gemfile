1a3174b7fe19ca1090a5bbccb041c3db05a05aa6
ecustovic <emina.custovic@edu.fit.ba>
2019-11-12 14:54:52 +0100

    Adding avatar for users

--- Gemfile
+++ Gemfile
@@ -25,9 +25,9 @@ gem 'bcrypt', '~> 3.1.7'
 gem 'bootstrap', '~> 4.3.1'
 gem 'devise', '~> 4.7', '>= 4.7.1'
 gem 'kaminari'
-
+gem 'gravatar_image_tag', github: 'mdeering/gravatar_image_tag'
 # Use Active Storage variant
-# gem 'image_processing', '~> 1.2'
+gem 'image_processing', '~> 1.2'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+      
   has_many :reviews, dependent: :destroy
   has_many :favourites, dependent: :destroy
   has_many :favourite_movies, through: :favourites, source: :movie
+  has_one_attached :avatar
 
   scope :by_name, -> { order(:name)}
   scope :not_admins, -> { by_name.where(admin: false)}
