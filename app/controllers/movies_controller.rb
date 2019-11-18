634fd392e36072674cfe1035bc7e9448707c1544
Dino Maric <dino.onex@gmail.com>
2019-11-18 10:55:04 +0100

    Added system test for visitor registration
    Basic start on system tests

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,73 +3,73 @@ class MoviesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :require_admin, except: [:index, :show]
   before_action :set_movie, only: [:show, :edit, :update, :destory]
-  
-    def index
-      case params[:filter]
-      when "upcoming"
-        @movies = Movie.upcoming.page(params["page"])
-      when "recent"
-        @movies = Movie.recent.page(params["page"])
-      when "flops"
-        @movies = Movie.flops.page(params["page"])
-      when "hits"
-        @movies = Movie.hits.page(params["page"])
-      else
-        @movies = Movie.released.page(params["page"])
-      end
-    end
 
-    def show
-      @fans = @movie.fans
-      @genres = @movie.genres
-      
-      if current_user
-        @favourite = current_user.favourites.find_by(movie_id: @movie.id)
-      end
+  def index
+    case params[:filter]
+    when "upcoming"
+      @movies = Movie.upcoming.page(params["page"])
+    when "recent"
+      @movies = Movie.recent.page(params["page"])
+    when "flops"
+      @movies = Movie.flops.page(params["page"])
+    when "hits"
+      @movies = Movie.hits.page(params["page"])
+    else
+      @movies = Movie.released.page(params["page"])
     end
+  end
 
-    def edit
-      @movie = Movie.find(params[:id]) 
+  def show
+    @fans = @movie.fans
+    @genres = @movie.genres
+
+    if current_user
+      @favourite = current_user.favourites.find_by(movie_id: @movie.id)
     end
+  end
 
-    def update
-        if @movie.update(movie_params)
-          redirect_to @movie, notice: "Movie successfully updated!"
-        else
-          render :edit
-        end
+  def edit
+    @movie = Movie.find(params[:id]) 
+  end
+
+  def update
+    if @movie.update(movie_params)
+      redirect_to @movie, notice: "Movie successfully updated!"
+    else
+      render :edit
     end
+  end
 
-    def new
-      @movie = Movie.new
-    end
+  def new
+    @movie = Movie.new
+  end
 
-    def create
-      @movie = Movie.new(movie_params)
-        if @movie.save
-          redirect_to @movie, notice: "Movie successfully created!"
-        else
-          render :new
-        end  
-    end
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to @movie, notice: "Movie successfully created!"
+    else
+      render :new
+    end  
+  end
 
-    def destroy
-      @movie.destroy
-      redirect_to movies_url, alert: "Movie successfully deleted!"
-    end
+  def destroy
+    @movie.destroy
+    redirect_to movies_url, alert: "Movie successfully deleted!"
+  end
 
-    
-    private
 
-    def set_movie
-      @movie = Movie.find_by!(slug: params[:id])
-    end
+  private
 
-    def movie_params 
-      movie_params =
-        params.require(:movie).
-                permit(:title, :rating, :total_gross, :description, :released_on,
-                :duration, :director, :image_file_name, genre_ids: [])
-    end
+  def set_movie
+    @movie = Movie.find_by!(slug: params[:id])
+  end
+
+  def movie_params 
+    movie_params =
+      params.require(:movie).
+      permit(:title, :rating, :total_gross, :description, :released_on,
+             :duration, :director, :image_file_name, genre_ids: [])
+  end
 
 end
