f6c51e6a5a9cf990277f40945e80631547ea2c96
ecustovic <emina.custovic@edu.fit.ba>
2019-11-18 11:29:55 +0100

    Created system tests for sign_in and sign_up

---
+++ test/system/sign_in_test.rb
@@ -0,0 +1,23 @@
+require "application_system_test_case"
+
+class SignInTest < ApplicationSystemTestCase
+  test "visitor can signin" do
+    visit new_user_session_path
+
+    fill_in "Email", with: users(:dino).email
+    fill_in "Password", with: "topsecret123"
+    click_on "Log in"
+
+    assert_text "Signed in successfully."
+  end
+
+  test "visitor can't signin" do
+    visit new_user_session_path
+
+    fill_in "Email", with: "novi_mail@gmail.com"
+    fill_in "Password", with: "123456"
+    click_on "Log in"
+
+    assert_text "Invalid Email or password."
+  end
+end
