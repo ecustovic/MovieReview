8978f479d5ddeeadd2a59bb873d5d9bdb13c1fa8
ecustovic <emina.custovic@edu.fit.ba>
2019-12-10 13:28:47 +0100

    Recreate movie Show page

--- app/helpers/favourites_helper.rb
+++ app/helpers/favourites_helper.rb
@@ -1,13 +1,13 @@
 module FavouritesHelper
   def fave_or_unfave_button(movie, favourite)
     if favourite
-      link_to  "♡ Unfave", favourite_path(id: movie.id), method: :delete, :class => "btn btn-primary"
+      link_to  "♡ Unfave", favourite_path(id: movie.id), method: :delete, :class => "btn-primary"
     else
-      link_to "❤ Fave", favourites_path(movie_id: movie.id), method: :post, :class => "btn btn-primary"
+      link_to "❤ Fave", favourites_path(movie_id: movie.id), method: :post, :class => "btn-primary"
     end
   end
 
   def unfave_button(movie)
-    link_to  "♡ Unfave", favourite_path(id: movie.id), method: :delete, :class => "btn btn-primary"
+    link_to  "♡ Unfave", favourite_path(id: movie.id), method: :delete, :class => "btn-primary"
   end
 end
